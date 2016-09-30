f7cb294397d1451a65fcfdb3f2db7f499cad7532
padamsson <patricia.adamsson@creuna.se>
2016-09-30 19:34:05 +0200

    Make units its own page

--- src/app.js
+++ src/app.js
@@ -1,47 +1,5 @@
-import {Unit} from './components/unit';
-
 export class App {
   constructor() {
-    this.heading = "Units";
-    this.units = [
-      {
-        "name": "Unit A",
-        "last_updated": "2015-01-13",
-        "id": 123555
-      },
-      {
-        "name": "Unit b",
-        "last_updated": "2015-01-13",
-        "id": 12355599
-      },
-      {
-        "name": "Unit C",
-        "last_updated": "2015-01-15",
-        "id": 12355577
-      },
-      {
-        "name": "Unit A1",
-        "last_updated": "2015-03-13",
-        "id": 123555111
-      },
-      {
-        "name": "Unit 1234",
-        "last_updated": "2016-01-20",
-        "id": 123555222
-      },
-      {
-        "name": "Unit ABC",
-        "last_updated": "2016-04-13",
-        "id": 12355544
-      }
-    ],
-    this.unitName = '';
-  }
-
-  addUnit() {
-    if (this.unitName) {
-      this.units.push(new Todo(this.unitName));
-      this.unitName = '';
-    }
+    
   }
 }
