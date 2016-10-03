2909fb8eda31b968220a6735dc12c58a46e53052
padamsson <patricia.adamsson@creuna.se>
2016-10-03 12:33:49 +0200

    Add missing files after sync error

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
+import environment from './environment';
+
+//Configure Bluebird Promises.
+//Note: You may want to use environment-specific configuration.
+Promise.config({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
+
 export function configure(aurelia) {
-  aurelia.use.basicConfiguration();
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
   aurelia.start().then(() => aurelia.setRoot());
 }
